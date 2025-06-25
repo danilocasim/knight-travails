c583d0ca3afb6d9b6f5b1bba7a18c2c6064bba2a
Danilo Casim Jr. <danilocasim174@gmail.com>
2025-06-25 20:32:15 +0800

    docs: sharing

--- algo.js
+++ algo.js
@@ -1,24 +1,19 @@
 function knightMoves(start, end) {
   const que = [];
-  const lastMoves = [];
+  let moves = [];
+
   que.push(start);
 
   while (
     !(que[que.length - 1][0] === end[0] && que[que.length - 1][1] === end[1])
   ) {
-    const moves = [];
-
     const current = que[0];
 
     moves.push(current);
-    lastMoves.push(moves[0]);
-
-    console.log(lastMoves);
 
     //FORWARD BIG RIGHT
     if (current[0] + 1 <= 7 && current[1] + 2 <= 7) {
       que.push([current[0] + 1, current[1] + 2]);
-      moves.push([current[0] + 1, current[1] + 2]);
     }
 
     if (
@@ -31,92 +26,78 @@ function knightMoves(start, end) {
     //FORWARD SMALL RIGHT
     if (current[0] + 2 <= 7 && current[1] + 1 <= 7) {
       que.push([current[0] + 2, current[1] + 1]);
-      moves.push([current[0] + 2, current[1] + 1]);
     }
 
     if (
       moves[moves.length - 1][0] === end[0] &&
       moves[moves.length - 1][1] === end[1]
     ) {
-      // lastMoves.push(moves);
       return moves;
     }
     //BACKWARD BIG RIGHT
     if (current[0] + 1 <= 7 && current[1] - 2 >= 0) {
       que.push([current[0] + 1, current[1] - 2]);
-      moves.push([current[0] + 1, current[1] - 2]);
     }
 
     if (
       moves[moves.length - 1][0] === end[0] &&
       moves[moves.length - 1][1] === end[1]
     ) {
-      // lastMoves.push(moves);
       return moves;
     }
     //BACKWARD SMALL RIGHT
     if (current[0] + 2 <= 7 && current[1] - 1 >= 0) {
       que.push([current[0] + 2, current[1] - 1]);
-      moves.push([current[0] + 2, current[1] - 1]);
     }
 
     if (
       moves[moves.length - 1][0] === end[0] &&
       moves[moves.length - 1][1] === end[1]
     ) {
-      // lastMoves.push(moves);
       return moves;
     }
     //BACKWARD BIG LEFT
     if (current[0] - 1 >= 0 && current[1] - 2 >= 0) {
       que.push([current[0] - 1, current[1] - 2]);
-      moves.push([current[0] - 1, current[1] - 2]);
     }
 
     if (
       moves[moves.length - 1][0] === end[0] &&
       moves[moves.length - 1][1] === end[1]
     ) {
-      // lastMoves.push(moves);
       return moves;
     }
     //BACKWARD SMALL LEFT
     if (current[0] - 2 >= 0 && current[1] - 1 >= 0) {
       que.push([current[0] - 2, current[1] - 1]);
-      moves.push([current[0] - 2, current[1] - 1]);
     }
 
     if (
       moves[moves.length - 1][0] === end[0] &&
       moves[moves.length - 1][1] === end[1]
     ) {
-      // lastMoves.push(moves);
       return moves;
     }
     //FORWARD SMALL LEFT
     if (current[0] - 2 >= 0 && current[1] + 1 <= 7) {
       que.push([current[0] - 2, current[1] + 1]);
-      moves.push([current[0] - 2, current[1] + 1]);
     }
 
     if (
       moves[moves.length - 1][0] === end[0] &&
       moves[moves.length - 1][1] === end[1]
     ) {
-      // lastMoves.push(moves);
       return moves;
     }
     //FORWARD BIG LEFT
     if (current[0] - 1 >= 0 && current[1] + 2 <= 7) {
       que.push([current[0] - 1, current[1] + 2]);
-      moves.push([current[0] - 1, current[1] + 2]);
     }
 
     if (
       moves[moves.length - 1][0] === end[0] &&
       moves[moves.length - 1][1] === end[1]
     ) {
-      // lastMoves.push(moves);
       return moves;
     }
 
@@ -124,4 +105,4 @@ function knightMoves(start, end) {
   }
 }
 
-console.log(knightMoves([0, 0], [3, 5]));
+console.log(knightMoves([0, 0], [4, 3]));
