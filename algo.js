ac6c944a8f0bcf4b0bab6bd39c49084016f15e14
Danilo Casim Jr. <danilocasim174@gmail.com>
2025-06-26 10:30:19 +0800

    feat: knightMoves function find the smallest move it takes from start to end using BFS for queing all moves and also save the connection between current node and next node to be able to backtrack the connections

--- algo.js
+++ algo.js
@@ -1,6 +1,6 @@
 function knightMoves(start, end) {
   const que = [];
-  let moves = [];
+  const connections = [];
 
   que.push(start);
 
@@ -9,100 +9,235 @@ function knightMoves(start, end) {
   ) {
     const current = que[0];
 
-    moves.push(current);
-
     //FORWARD BIG RIGHT
     if (current[0] + 1 <= 7 && current[1] + 2 <= 7) {
+      connections.push({
+        current: current,
+        next: [current[0] + 1, current[1] + 2],
+      });
+
       que.push([current[0] + 1, current[1] + 2]);
     }
 
     if (
-      moves[moves.length - 1][0] === end[0] &&
-      moves[moves.length - 1][1] === end[1]
+      que[que.length - 1][0] === end[0] &&
+      que[que.length - 1][1] === end[1]
     ) {
-      return moves;
+      const mid = [end];
+      connections.reverse();
+
+      let next = end;
+
+      connections.forEach((path) => {
+        if (path.next[0] === next[0] && path.next[1] === next[1]) {
+          next = path.current;
+          mid.push(path.current);
+        }
+      });
+
+      return mid.reverse();
     }
 
     //FORWARD SMALL RIGHT
     if (current[0] + 2 <= 7 && current[1] + 1 <= 7) {
+      connections.push({
+        current: current,
+        next: [current[0] + 2, current[1] + 1],
+      });
+
       que.push([current[0] + 2, current[1] + 1]);
     }
 
     if (
-      moves[moves.length - 1][0] === end[0] &&
-      moves[moves.length - 1][1] === end[1]
+      que[que.length - 1][0] === end[0] &&
+      que[que.length - 1][1] === end[1]
     ) {
-      return moves;
-    }
-    //BACKWARD BIG RIGHT
-    if (current[0] + 1 <= 7 && current[1] - 2 >= 0) {
-      que.push([current[0] + 1, current[1] - 2]);
+      const mid = [end];
+      connections.reverse();
+
+      let next = end;
+
+      connections.forEach((path) => {
+        if (path.next[0] === next[0] && path.next[1] === next[1]) {
+          next = path.current;
+          mid.push(path.current);
+        }
+      });
+      return mid.reverse();
     }
 
-    if (
-      moves[moves.length - 1][0] === end[0] &&
-      moves[moves.length - 1][1] === end[1]
-    ) {
-      return moves;
-    }
+    // Other moves ommitted for brevity
+
     //BACKWARD SMALL RIGHT
     if (current[0] + 2 <= 7 && current[1] - 1 >= 0) {
+      connections.push({
+        current: current,
+        next: [current[0] + 2, current[1] - 1],
+      });
+
       que.push([current[0] + 2, current[1] - 1]);
     }
 
     if (
-      moves[moves.length - 1][0] === end[0] &&
-      moves[moves.length - 1][1] === end[1]
+      que[que.length - 1][0] === end[0] &&
+      que[que.length - 1][1] === end[1]
     ) {
-      return moves;
+      const mid = [end];
+      connections.reverse();
+
+      let next = end;
+
+      connections.forEach((path) => {
+        if (path.next[0] === next[0] && path.next[1] === next[1]) {
+          next = path.current;
+          mid.push(path.current);
+        }
+      });
+      return mid.reverse();
     }
+
+    //BACKWARD BIG RIGHT
+    if (current[0] + 1 <= 7 && current[1] - 2 >= 0) {
+      connections.push({
+        current: current,
+        next: [current[0] + 1, current[1] - 2],
+      });
+
+      que.push([current[0] + 1, current[1] - 2]);
+    }
+
+    if (
+      que[que.length - 1][0] === end[0] &&
+      que[que.length - 1][1] === end[1]
+    ) {
+      const mid = [end];
+      connections.reverse();
+
+      let next = end;
+
+      connections.forEach((path) => {
+        if (path.next[0] === next[0] && path.next[1] === next[1]) {
+          next = path.current;
+          mid.push(path.current);
+        }
+      });
+      return mid.reverse();
+    }
+
     //BACKWARD BIG LEFT
     if (current[0] - 1 >= 0 && current[1] - 2 >= 0) {
+      connections.push({
+        current: current,
+        next: [current[0] - 1, current[1] - 2],
+      });
+
       que.push([current[0] - 1, current[1] - 2]);
     }
 
     if (
-      moves[moves.length - 1][0] === end[0] &&
-      moves[moves.length - 1][1] === end[1]
+      que[que.length - 1][0] === end[0] &&
+      que[que.length - 1][1] === end[1]
     ) {
-      return moves;
+      const mid = [end];
+      connections.reverse();
+
+      let next = end;
+
+      connections.forEach((path) => {
+        if (path.next[0] === next[0] && path.next[1] === next[1]) {
+          next = path.current;
+          mid.push(path.current);
+        }
+      });
+      return mid.reverse();
     }
+
     //BACKWARD SMALL LEFT
     if (current[0] - 2 >= 0 && current[1] - 1 >= 0) {
+      connections.push({
+        current: current,
+        next: [current[0] - 2, current[1] - 1],
+      });
+
       que.push([current[0] - 2, current[1] - 1]);
     }
 
     if (
-      moves[moves.length - 1][0] === end[0] &&
-      moves[moves.length - 1][1] === end[1]
+      que[que.length - 1][0] === end[0] &&
+      que[que.length - 1][1] === end[1]
     ) {
-      return moves;
+      const mid = [end];
+      connections.reverse();
+
+      let next = end;
+
+      connections.forEach((path) => {
+        if (path.next[0] === next[0] && path.next[1] === next[1]) {
+          next = path.current;
+          mid.push(path.current);
+        }
+      });
+      return mid.reverse();
     }
+
     //FORWARD SMALL LEFT
     if (current[0] - 2 >= 0 && current[1] + 1 <= 7) {
+      connections.push({
+        current: current,
+        next: [current[0] - 2, current[1] + 1],
+      });
+
       que.push([current[0] - 2, current[1] + 1]);
     }
 
     if (
-      moves[moves.length - 1][0] === end[0] &&
-      moves[moves.length - 1][1] === end[1]
+      que[que.length - 1][0] === end[0] &&
+      que[que.length - 1][1] === end[1]
     ) {
-      return moves;
+      const mid = [end];
+      connections.reverse();
+
+      let next = end;
+
+      connections.forEach((path) => {
+        if (path.next[0] === next[0] && path.next[1] === next[1]) {
+          next = path.current;
+          mid.push(path.current);
+        }
+      });
+      return mid.reverse();
     }
+
     //FORWARD BIG LEFT
     if (current[0] - 1 >= 0 && current[1] + 2 <= 7) {
+      connections.push({
+        current: current,
+        next: [current[0] - 1, current[1] + 2],
+      });
+
       que.push([current[0] - 1, current[1] + 2]);
     }
 
     if (
-      moves[moves.length - 1][0] === end[0] &&
-      moves[moves.length - 1][1] === end[1]
+      que[que.length - 1][0] === end[0] &&
+      que[que.length - 1][1] === end[1]
     ) {
-      return moves;
+      const mid = [end];
+      connections.reverse();
+
+      let next = end;
+
+      connections.forEach((path) => {
+        if (path.next[0] === next[0] && path.next[1] === next[1]) {
+          next = path.current;
+          mid.push(path.current);
+        }
+      });
+      return mid.reverse();
     }
 
     que.shift();
   }
 }
 
-console.log(knightMoves([0, 0], [4, 3]));
+console.log(knightMoves([0, 0], [7, 7]));
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,2 +1,2 @@
-    // if (que[que.length - 1][0] === end[0] && que[que.length - 1][1] === end[1])
-    //   return que;
+
+      connections.forEach((path) => {
